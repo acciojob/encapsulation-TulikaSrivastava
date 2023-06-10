7c9bd97c5f90f5035287321f643f76ece08b0f18
Tulika <sriva0507@gmail.com>
2023-06-10 18:32:46 +0530

    Add Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,7 @@
 package com.driver;
 
 public class Main {
-    public static void (String[] args){
+    public static void  main(String[] args){
         RWOnly obj=new RWOnly();
         //obj.name="Tulika";
         obj.setName("Tulika");
