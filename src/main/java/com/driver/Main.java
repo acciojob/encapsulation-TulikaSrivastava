bb10cae01250bf747e6dfdd7d401e08c5bdc3655
Tulika <sriva0507@gmail.com>
2023-06-10 18:06:58 +0530

    Added soluton

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void (String[] args){
+        RWOnly obj=new RWOnly();
+        //obj.name="Tulika";
+        obj.setName("Tulika");
+        obj.getName();
+    }
   
 }
